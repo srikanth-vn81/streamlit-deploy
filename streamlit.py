26ba5b46fe31418e744935060a2a245d4526d5eb
srikanth-vn81 <68362977+srikanth-vn81@users.noreply.github.com>
2023-03-28 11:42:06 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+import streamlit as st
+
+
+# In[3]:
+
+
+#df = pd.read_csv(r'C:\Users\srikanthve\Documents\Maria DB\week13.csv')
+
+
+# In[4]:
+
+
+#df.head()
+
+
+# In[5]:
+
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+
+# In[7]:
+
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    st.write(df)
+
+
+# In[ ]:
+
+
+
+
